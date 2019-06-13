cf06457b148adce5777b501f170f97a658de1baf
Keith Dahlby <keith@newbo.co>
2019-06-13 11:09:25 -0500

    Add connection/socket pool

--- curriculum/class-17/demo/server/server.js
+++ curriculum/class-17/demo/server/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const net = require('net');
+const uuid = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 const server = net.createServer();
@@ -9,11 +10,20 @@ server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
+let socketPool = {};
+
 server.on('connection', socket => {
+  let id = uuid();
+  socket.id = id;
+  socketPool[id] = socket;
+
+  console.log(`Connection count: ${Object.keys(socketPool).length}`);
+
   socket.on('data', buffer => {
-    console.log(buffer.toString().trim());
+    console.log(id, buffer.toString().trim());
   });
   socket.on('close', () => {
-    console.log('closing!');
+    console.log(id, 'closing!');
+    delete socketPool[id];
   });
 });
