1d1ba97923bc53459541472bc3ea020c0f547a7b
Keith Dahlby <keith@newbo.co>
2019-06-13 11:25:43 -0500

    Add logger that connects to server

--- curriculum/class-17/demo/server/server.js
+++ curriculum/class-17/demo/server/server.js
@@ -19,11 +19,22 @@ server.on('connection', socket => {
 
   console.log(`Connection count: ${Object.keys(socketPool).length}`);
 
-  socket.on('data', buffer => {
-    console.log(id, buffer.toString().trim());
-  });
+  for (let socketId in socketPool) {
+    if (socketId === id) continue;
+
+    socketPool[socketId].write(`${id} connected!\r\n`);
+  }
+
+  socket.on('data', dataHandler);
   socket.on('close', () => {
     console.log(id, 'closing!');
     delete socketPool[id];
   });
 });
+
+// TODO: move this into a separate module
+// TODO: add tests that have nothing to do with the socket
+// NOTE: use call/bind/apply to specify 'this' in tests!
+function dataHandler(buffer) {
+  console.log(this.id, buffer.toString().trim());
+}
