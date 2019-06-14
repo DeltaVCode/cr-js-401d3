826a22945ff7bf62575b024f7452e2509d131817
Keith Dahlby <keith@newbo.co>
2019-06-14 11:35:56 -0500

    Add some middleware

--- curriculum/class-20/demo/rebuild-api/lib/app.js
+++ curriculum/class-20/demo/rebuild-api/lib/app.js
@@ -3,6 +3,15 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
 app.use(require('../routes/home-routes'));
+
+// Parse bodies after home routes that don't need them
+app.use(express.json());
 
 module.exports = app;
